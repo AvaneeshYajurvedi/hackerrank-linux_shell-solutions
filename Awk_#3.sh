65f75877a9e8c4ef57ca6aef4c01bdbabc3f14ce
Avaneesh Yajurvedi <aviiy1710@gmail.com>
2025-11-29 19:05:54 +0530

    Added Awk #3 Solution

---
+++ Awk_#3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+awk '{
+avg = ($2 + $3 + $4) / 3
+if (avg >= 80){
+print $1 " " $2 " " $3 " " $4 " : A";}
+else if (avg >= 60){
+print $1 " " $2 " " $3 " " $4 " : B";}
+else{
+print $1 " " $2 " " $3 " " $4 " : FAIL";}
+}'
